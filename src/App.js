ee329d3955c55260dca8749a309b64eb28ccad27
Guillermo Zevallos <guiller.zeva16@gmail.com>
2022-08-08 20:26:41 -0500

    Create deck of cards

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Card from './components/Card';
 import './SCSS/App.scss';
 
 const characters = require('./assets/data.json'); 
 function App() {
 
+  const [deckOfCards, setDeckOfCards] = useState([]);
+
   useEffect(()=>{
-    console.log(characters);
-  })
+    setDeckOfCards(shuffle(characters));
+  },[])
 
   return (
     <div className="App">
       {
-        characters.map( c => <Card character={c} key={c.name} /> )
+        deckOfCards.map( c => <Card character={c} key={c.name} /> )
       }
     </div>
   );
+}
+
+function shuffle(characters){
+  let newArr = [];
+
+  for(let i = 0; i < 3; i++){
+    let randomNumber;
+    let isInclude = false;
+
+    while(!isInclude){
+      randomNumber = Math.floor(Math.random() * characters.length);
+      if(!newArr.includes(characters[randomNumber])){
+        isInclude = true;
+      }
+    }
+
+    newArr.push(characters[randomNumber]);
+  }
+  return newArr;
 }
 
 export default App;
